58783f992aa002fbacab1b2e02b59cdff5761bde
ijneb <senochan@autistici.org>
2018-11-09 00:18:42 +0000

    add gh & li icons

--- src/inner.nim
+++ src/inner.nim
@@ -26,8 +26,17 @@ proc index(): string =
           h1("About Me"),
           p("I'm a hobbyist woodworker, a capricious musician, and an autodidactic lover of mathematics and physics who is commonly working toward computational solutions."),
           h1("Contact"),
-          ul(li("Discord: benji#4364"), li("XMPP: benji@wusz.org")),
-          p("Connect with me on ", a(href="https://www.linkedin.com/in/benjamin-frady", "Linkedin"), " or browse the projects that I decide to host on ", a(href="https://github.com/ijneb", "GitHub"), "."),
+          ul(
+            li("Email: ", a(href="mailto:benji@codewat.ch", "benji@codewat.ch")),
+            li("Discord: benji#4364"),
+            li("XMPP: benji@wusz.org")
+          ),
+          p(
+            "Connect with me on ",
+            a(href="https://www.linkedin.com/in/benjamin-frady", img(src="/icons/linkedin.svg", alt="", width="16px", class="icon"), "Linkedin"),
+            " or browse the works that I put on ",
+            a(href="https://github.com/ijneb", img(src="/icons/github.svg", alt="", width="16px", class="icon"), "GitHub"), "."
+          ),
           p(a(href="https://github.com/ijneb/nim-blog", "Browse the source code for this website here."))
         ),
         `div`(id="right",
